6b97fa6c580c30890b3f60ba92b5e260392744fd
Gabry17 <gbianchi528@gmail.com>
2022-04-07 15:58:13 +0200

    aggiunto sconto del 20% , sconto del 40% le condizioni in cui applicare gli sconti e la visualuzzazione del prezzo finale nel browser

--- js/script.js
+++ js/script.js
@@ -7,8 +7,25 @@ console.log(km, typeof(km));
 const age = parseInt(prompt('Quanti anni hai?'));
 console.log(age, typeof(age));
 
+//ELABORAZIONE
 //calcolare prezzo biglietto (0,21€ / km)
+const normalPrice = 0.21 * km;
+console.log(normalPrice);
 //se la persona a meno di 18 anni sconto 20%
 //se e over 65 sconto 40%
 
+let finalPrice;
+let sale20 = normalPrice * 20 / 100;
+let sale40 = normalPrice * 40 / 100;
+
+if (age < 18) {
+    finalPrice = normalPrice - sale20;
+} else if (age > 65) {
+    finalPrice = normalPrice - sale40;
+}else {
+    finalPrice = normalPrice;
+}
+
+
 //output
+document.getElementById('ticketPrice').innerHTML = finalPrice.toFixed(2);
