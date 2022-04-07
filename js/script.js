858a5f7eb9c9dab4399bd30ee855fb4030a688e3
Gabry17 <gbianchi528@gmail.com>
2022-04-07 15:17:15 +0200

    aggiunto due prompt

---
+++ js/script.js
@@ -0,0 +1,14 @@
+//DICHIARAZIONE
+//chiedere km da percorrere
+//chiedere eta
+const km = parseInt(prompt('Quanti km vuoi percorrere?'));
+console.log(km, typeof(km));
+
+const age = parseInt(prompt('Quanti anni hai?'));
+console.log(age, typeof(age));
+
+//calcolare prezzo biglietto (0,21€ / km)
+//se la persona a meno di 18 anni sconto 20%
+//se e over 65 sconto 40%
+
+//output
